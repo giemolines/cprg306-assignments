0c84276239f358df55fc79fa5926aa0fd05f1e7c
cam_sanjuan <135734842+giemolines@users.noreply.github.com>
2024-05-13 14:41:22 -0600

    update#2
    main page

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="./week-2"></Link>
+      <Link href="./week-2">Week 2</Link>
     </main>
   );
 }
