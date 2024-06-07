146971bc89c35b127f2438052dd18e5eca7a8341
cam_sanjuan <135734842+giemolines@users.noreply.github.com>
2024-06-07 16:02:44 -0600

    added optional group by

--- app/week-5/item-list.js
+++ app/week-5/item-list.js
@@ -6,8 +6,9 @@ import Item from './item';
 
 export default function ItemList(){
     const [sortBy, setSortBy] = useState("name");
+    const [grouped, setGrouped] = useState(false);
     
-    function sortItems (items){
+    function sortItems (){
         if (sortBy == "name"){
             items.sort((a, b) =>  a.name.localeCompare(b.name));
         }
@@ -20,23 +21,67 @@ export default function ItemList(){
 
     const sortByName = () => {
         setSortBy("name");
+        setGrouped(false);
         console.log(sortBy);
     }
 
     const sortByCategory = () => {
         setSortBy("category");
+        setGrouped(false);
         console.log(sortBy);
+    }
+
+    const groupByCategory = () => {
+        setSortBy("category");
+        setGrouped(true);
+        console.log(grouped);
     }
 
     const displayItems = (items) =>{
         const sortedItems = sortItems(items)
-        return sortedItems.map((item) => 
-            <li key = {item.id}>
-                 <Item name={item.name} quantity={item.quantity} category={item.category}/>
-            </li>);
+        const categories =[];
+
+        if (!grouped){
+            return sortedItems.map((item) => 
+                <li key = {item.id}>
+                     <Item name={item.name} quantity={item.quantity} category={item.category}/>
+                </li>);
+        }
+
+        else{
+            sortedItems.forEach(item => {
+                if (!categories.includes(item.category)){
+                    categories.push(item.category)
+                }
+            });
+
+            return categories.map((category) => (
+                <div key ={category}>
+                    <div className='text-stone-50 text-2xl capitalize font-semibold'>
+                        <h2>{category}</h2>
+                    </div>
+                    <ul>
+                    {sortedItems.filter((item) => item.category === category)
+                        .map((item) => (
+                            <li key={item.id}>
+                                <Item name={item.name} quantity={item.quantity} category={item.category}/>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))
+
+
+        }
     }
 
+    // GROUP BY CATEGORY
+
     
+
+    
+
+
 
     return(
         <main>
@@ -57,7 +102,7 @@ export default function ItemList(){
                         </button>
                     </div>
                     <div>
-                        <button className="flex-1 text-xl text-stone-50  bg-orange-400 rounded-xl p-3 hover:bg-orange-800 active:bg-yellow-400">
+                        <button className="flex-1 text-xl text-stone-50  bg-orange-400 rounded-xl p-3 hover:bg-orange-800 active:bg-yellow-400" onClick={groupByCategory}>
                             Group By Category
                         </button>
                     </div>
@@ -67,8 +112,7 @@ export default function ItemList(){
                 <ul>
                     {displayItems(items)}
                 </ul>
-            </div>
-            
+            </div>   
     </main>
 
     );
